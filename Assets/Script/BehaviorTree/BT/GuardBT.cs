885fde5625e47667a15f75d90848e52b0406a694
Songryan <tjr6255@gmail.com>
2024-07-07 21:36:07 +0900

    [Update] Patrol > Attack > Die 까지 구현.

--- Assets/Script/BehaviorTree/BT/GuardBT.cs
+++ Assets/Script/BehaviorTree/BT/GuardBT.cs
@@ -8,11 +8,16 @@ public class GuardBT : Tree
     public static float speed = 2f;
     public static float Runspeed = 4f;
     public static float fovRange = 10f;
+    public static float attackRange = 1f;
 
     protected override Node SetupTree()
     {
         Node root = new Selector(new List<Node>
         {
+            new Sequence(new List<Node>{
+                new CheckEnemyInAttackRange(transform),
+                new TaskAttack(transform),
+            }),
             new Sequence(new List<Node>{ 
                 new CheckEnemyInFOVRange(transform),
                 new TaskGoToTarget(transform),
--- Assets/Script/BehaviorTree/SubTask/CheckEnemyInAttackRange.cs
+++ Assets/Script/BehaviorTree/SubTask/CheckEnemyInAttackRange.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using BehaviorTree;
 using UnityEngine;
 
-public class CheckEnemyInAttackRange : MonoBehaviour
+public class CheckEnemyInAttackRange : Node
 {
-    // Start is called before the first frame update
-    void Start()
+    private static int _enemyLayerMask = LayerMask.GetMask("Player");
+
+    private Transform _transform;
+    private Animator _animator;
+
+    public CheckEnemyInAttackRange(Transform transform)
     {
-        
+        _transform = transform;
+        _animator = transform.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override NodeState Evaluate()
     {
+        object t = GetData("target");
+
+        if (t == null)
+        {
+            state = NodeState.FAILURE;
+            return state;
+        }
         
+        Transform target = (Transform)t;
+        if(Vector3.Distance(_transform.position, target.position) <= GuardBT.attackRange)
+        {
+            _animator.SetBool("Attacking", true);
+            _animator.SetBool("Walking", false);
+            _animator.SetBool("Run", false);
+
+            state = NodeState.SUCCESS;
+            return state;
+        }
+
+        _animator.SetBool("Attacking", false);
+
+        state = NodeState.FAILURE;
+        return state;
     }
 }
